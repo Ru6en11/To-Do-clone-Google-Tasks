48a61498c0cca9be88b18688a978896b9ec1ce5c
krasn <krasnoschek003@gmail.com>
2023-01-05 15:44:43 +1000

    delete unused import

--- app/src/main/java/com/example/googletasksclone/views/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/googletasksclone/views/tasks/TasksFragment.kt
@@ -2,7 +2,6 @@ package com.example.googletasksclone.views.tasks
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
